9cd498a8d1cfa3486a89be58650226179e675eb1
yamachoo <yamachoo567@gmail.com>
2020-07-04 11:05:29 +0900

    add emotion

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,7 +1,14 @@
+/** @jsx jsx */
 import React from "react"
+import { css, jsx } from "@emotion/core"
+
+const style = css({
+  margin: `0 auto`,
+  maxWidth: 800
+})
 
 const Layout: React.FC = ({ children }) => {
-  return <div>{children}</div>
+  return <div css={style}>{children}</div>
 }
 
 export default Layout
