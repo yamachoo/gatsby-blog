7635d12cb37db53a924f72bab65fb74eea9c583e
yamachoo <yamachoo567@gmail.com>
2020-07-18 13:57:35 +0900

    fix header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,38 +8,55 @@ export interface ISitePath {
   sitePath: string
 }
 
-const Header: React.FC<ISitePath> = ({ sitePath }) => (
-  <header className={styles.header}>
-    <div className={styles.layout}>
-      {sitePath === `/` ? (
-        <h1 className={styles.title}>
-          <Link to="/">Yamablog</Link>
-        </h1>
-      ) : (
-        <div className={styles.title}>
-          <Link to="/">Yamablog</Link>
-        </div>
-      )}
-      <div>
-        <a
-          className={styles.icon}
-          href="https://twitter.com/yamachoo567"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={twitter} alt="twitter-logo" />
-        </a>
-        <a
-          className={styles.icon}
-          href="https://github.com/yamachoo"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={github} alt="github-logo" />
-        </a>
+const Header: React.FC<ISitePath> = ({ sitePath }) => {
+  const socials = [
+    {
+      name: `twitter`,
+      href: `https://twitter.com/yamachoo567`,
+      src: twitter,
+      alt: `twitter-logo`
+    },
+    {
+      name: `github`,
+      href: `https://github.com/yamachoo`,
+      src: github,
+      alt: `github-logo`
+    }
+  ]
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.layout}>
+        {sitePath === `/` ? (
+          <h1 className={styles.title}>
+            <Link to="/">Yamablog</Link>
+          </h1>
+        ) : (
+          <div className={styles.title}>
+            <Link to="/">Yamablog</Link>
+          </div>
+        )}
+        <ul className={styles.socials}>
+          {socials.map((social) => (
+            <li key={social.name}>
+              <a
+                className={styles.link}
+                href={social.href}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  className={styles.icon}
+                  src={social.src}
+                  alt={social.alt}
+                />
+              </a>
+            </li>
+          ))}
+        </ul>
       </div>
-    </div>
-  </header>
-)
+    </header>
+  )
+}
 
 export default Header
