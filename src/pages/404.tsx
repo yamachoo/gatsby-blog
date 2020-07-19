90632ef0b2c5a444ab6753f425f1c8452eb55330
yamachoo <yamachoo567@gmail.com>
2020-07-19 10:38:03 +0900

    update 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,12 +1,21 @@
 import React from "react"
 import { PageProps } from "gatsby"
 import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 const Error404Page: React.FC<PageProps> = ({ path }) => {
   return (
     <Layout sitePath={path}>
-      <h1>You are here!</h1>
-      <h2>But nothing found for you #404</h2>
+      <SEO title="404" />
+      <div className="article__title">
+        <h1>404 Not Found</h1>
+      </div>
+      <div className="article__content">
+        <p>
+          お探しのページは見つかりませんでした。<br />
+          一時的にアクセスできない状態か、移動もしくは削除された可能性があります。
+        </p>
+      </div>
     </Layout>
   )
 }
