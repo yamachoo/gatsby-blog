be65248fce4b4a57d454f3e09c74744e98bc990e
yamachoo <yamachoo567@gmail.com>
2020-07-30 19:03:45 +0900

    delete Layout AdSense

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,7 +3,6 @@ import styles from "./layout.module.scss"
 import Header from "./header"
 import { ISitePath } from "./header"
 import Main from "./main"
-import AdSense from "./adSense"
 import Footer from "./footer"
 
 const Layout: React.FC<ISitePath> = ({ children, sitePath }) => {
@@ -12,7 +11,6 @@ const Layout: React.FC<ISitePath> = ({ children, sitePath }) => {
       <Header sitePath={sitePath} />
       <div className={styles.content}>
         <Main>{children}</Main>
-        <AdSense className={styles.adsense} />
       </div>
       <Footer />
     </div>
