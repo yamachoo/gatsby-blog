df75464a9c56dc2473ecd01ee1132e17ac554dc7
yamachoo <yamachoo567@gmail.com>
2020-12-30 22:46:20 +0900

    add AdSense

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,6 +3,7 @@ import styles from "./layout.module.scss"
 import Header from "./header"
 import { ISitePath } from "./header"
 import Main from "./main"
+import AdSense from "./adSense"
 import Footer from "./footer"
 
 const Layout: React.FC<ISitePath> = ({ children, sitePath }) => {
@@ -11,6 +12,7 @@ const Layout: React.FC<ISitePath> = ({ children, sitePath }) => {
       <Header sitePath={sitePath} />
       <div className={styles.content}>
         <Main>{children}</Main>
+        <AdSense className={styles.adsense} />
       </div>
       <Footer />
     </div>
